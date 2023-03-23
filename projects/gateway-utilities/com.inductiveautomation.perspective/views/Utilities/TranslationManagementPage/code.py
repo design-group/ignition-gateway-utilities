218c4dffbd15c095a18050ad9b0d7ebb982e540f
Amy <afelix51228@gmail.com>
2023-03-23 14:43:14 -0700

    implemented the translation management page

---
+++ projects/gateway-utilities/com.inductiveautomation.perspective/views/Utilities/TranslationManagementPage/code.py
@@ -0,0 +1,34 @@
+
+def import_template(view, event):
+	"""
+	DESCRIPTION: converts a template file from csv into json, and displays it in a preview table
+	PARAMETERS: view (object): the form that is importing the file, event(object): the file to upload
+	RETURNS: null
+	"""
+	file_string = event.file.getString()
+	view.view.custom.data = []
+	for line in file_string.split("\n")[1:]:
+		line = line.split(",")
+		view.view.custom.data.append({
+			"English": line[0],
+			"Spanish": line[1],
+		})
+	
+	
+	view.view.custom.uploaded = True
+
+def download_template_file(data):
+	"""
+	DESCRIPTION: downloads a file that represents all the setpoints in a dashboard as a CSV(but not their positions)
+	PARAMETERS: view (object): the carousel that holds all the pages of the template
+	RETURNS: null
+	"""
+	data = General.Conversion.convert_dataset_to_list(data)
+	translations = "English, Spanish\n"
+	for english_spanish in data:
+		system.perspective.print(data)
+		translations += "%s,%s\n"%(
+			english_spanish["English"],
+			english_spanish["Spanish"]
+		)
+	system.perspective.download("translation.csv", translations)
