1ebd0f048b78b9062081f2e06e0a2af7173298e4
Amy <afelix51228@gmail.com>
2023-03-31 13:04:45 -0700

    added updated version of translation page

--- projects/gateway-utilities/ignition/script-python/General/TranslationManagementPage/code.py
+++ projects/gateway-utilities/ignition/script-python/General/TranslationManagementPage/code.py
@@ -1,7 +1,4 @@
-"""
-General.Translation Management Page
-DESCRIPTION: This page is used to manage translations for a project
-"""
+
 def import_template(view, event):
 	"""
 	DESCRIPTION: converts a template file from csv into json, and displays it in a preview table
@@ -12,14 +9,15 @@ def import_template(view, event):
 	view.view.custom.data = []
 	for line in file_string.split("\n")[1:]:
 		line = line.split(",")
+		
 		view.view.custom.data.append({
 			"English": line[0],
-			"Spanish": line[1],
+			"Spanish": line[1]
 		})
-	
-	
+		system.util.modifyTranslation(line[0], line[1], "es")
 	view.view.custom.uploaded = True
-
+	
+	
 def download_template_file(data):
 	"""
 	DESCRIPTION: downloads a file that represents all the setpoints in a dashboard as a CSV(but not their positions)
@@ -29,9 +27,13 @@ def download_template_file(data):
 	data = General.Conversion.convert_dataset_to_list(data)
 	translations = "English, Spanish\n"
 	for english_spanish in data:
+#		system.perspective.print(data)
 		translations += "%s,%s\n"%(
 			english_spanish["English"],
 			english_spanish["Spanish"]
 		)
 	system.perspective.download("translation.csv", translations)
-
+	
+def add_entry(en, es): # get data from add entry page 
+	pass
+	
