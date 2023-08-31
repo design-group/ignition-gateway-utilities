b98c42b4121043cde1043812fd4c3fff7daab016
tyler-kaim1 <Tyler.Kaim@BWDesignGroup.com>
2023-08-31 11:43:24 -0700

    Fixing automated testing

--- tests/test_resource_files.py
+++ tests/test_resource_files.py
@@ -18,15 +18,25 @@ def test_resource():
 	# NOTE: Walk through each directory
 	for root_name, dir_names, file_names in os.walk(path): # pylint: disable=unused-variable
 		# NOTE: Load the resource.json into an object
-		if 'resource.json' in file_names:
+		if "resource.json" in file_names:
 			with open("%s/resource.json" % root_name) as json_file:
-				resource_data = json.load(json_file)
+				try:
+					resource_data = json.load(json_file)	    
+				except: # pylint: disable=bare-except
+					pytest.fail('Invalid resource file: %s/resource.json' % (root_name))
 
 			# NOTE: Go through each file in the directory
-			for f_name in file_names:
+			for os_file_name in file_names:
 				# NOTE: Verify that the file is in the resource.json.files
-				if f_name not in resource_data.get('files', []) and not any(ignore_text in f_name for ignore_text in ignore):
-					pytest.fail('%s not found in %s/resource.json' % (f_name, root_name))
+				if os_file_name not in resource_data.get("files", []) and not any(ignore_text in os_file_name for ignore_text in ignore):
+					pytest.fail("%s not found in %s/resource.json" % (os_file_name, root_name))
+
+			# NOTE: Go through each file in the resource.json. files object
+			for object_file_name in resource_data.get("files", []):
+				# NOTE: Verify that the file is in the directory
+				if object_file_name not in file_names and not any(ignore_text in object_file_name for ignore_text in ignore):
+					pytest.fail("%s not found in %s" % (object_file_name, root_name))
+
 
 if __name__ == "__main__":
 	pytest.main(["-s", __file__])
